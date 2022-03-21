975037e9590593ae1120f5bee9ece869895aa370
Thiago Cavalcante <thiagocavalcante@zup.com.br>
2022-03-21 10:59:50 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/spring/data/jpa/AluraSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/spring/data/jpa/AluraSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
