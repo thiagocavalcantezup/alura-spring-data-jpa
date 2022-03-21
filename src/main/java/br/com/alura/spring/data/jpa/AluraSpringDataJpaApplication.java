e0f7edafd6efbdd041a54f59e5bf7e74ce853926
Thiago Cavalcante <thiagocavalcante@zup.com.br>
2022-03-21 14:14:24 -0300

    Use the CargoRepository to save a new Cargo object
    Changes:
    - Repository dependency injection with constructor
    - Implementation of the CommandLineRunner interface to run commands
      after app initialization

--- src/main/java/br/com/alura/spring/data/jpa/AluraSpringDataJpaApplication.java
+++ src/main/java/br/com/alura/spring/data/jpa/AluraSpringDataJpaApplication.java
@@ -1,13 +1,29 @@
 package br.com.alura.spring.data.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AluraSpringDataJpaApplication {
+import br.com.alura.spring.data.jpa.models.Cargo;
+import br.com.alura.spring.data.jpa.repositories.CargoRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AluraSpringDataJpaApplication.class, args);
-	}
+@SpringBootApplication
+public class AluraSpringDataJpaApplication implements CommandLineRunner {
+
+    private final CargoRepository cargoRepository;
+
+    public AluraSpringDataJpaApplication(CargoRepository cargoRepository) {
+        this.cargoRepository = cargoRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraSpringDataJpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cargo cargo = new Cargo("DESENVOLVEDOR DE SOFTWARE");
+        cargoRepository.save(cargo);
+    }
 
 }
